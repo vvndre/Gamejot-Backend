6d83cf1112556df827d2f534aaffeab42060fca4
andrercastillon <andrercastillon@gmail.com>
2023-07-17 20:56:06 -0500

    gamejot init

---
+++ src/main.js
@@ -0,0 +1,16 @@
+let express = require("express");
+
+let app = express();
+
+require("dotenv").config();
+
+app.use(express.json());
+
+let PORT = process.env.PORT || 3000;
+
+let routes = require("./routes");
+app.use(routes);
+
+app.listen(PORT, function () {
+  console.log("Gamejot is Locked & Loaded", PORT);
+});
---
+++ src/model/tables.sql
@@ -0,0 +1,17 @@
+create table users (
+  user_id int primary key auto_increment,
+  username varchar(26) not null unique,
+  email varchar(255) not null unique,
+  password varchar(255),
+  created_at timestamp
+);
+
+create table posts (
+  post_id int primary key auto_increment,
+  user_id int,
+  game_title varchar(255),
+  game_year int,
+  game_dev varchar(255),
+  status enum ('completed', 'started', 'watchlist'),
+  created_at timestamp
+);
